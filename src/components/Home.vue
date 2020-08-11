5272a120350c0ad0b0a1727ee059f0433244003d
hhg-ngu <ngu_hhg@sina.com>
2020-08-11 17:49:07 +0800

    侧边栏完成

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,19 +1,111 @@
 <template>
-  <div>
-    <el-button type="info" @click="logout">退出</el-button>
-    home
-  </div>
+  <el-container>
+    <el-header>
+      <div>后台管理系统</div>
+      <el-button type="info" @click="logout">退出</el-button></el-header
+    >
+    <el-container>
+      <el-aside :width="isCollapse ? '64px' : '200px'">
+        <div class="toggle_btn" @click="toggleCollapse">|||</div>
+        <el-menu
+          background-color="#333744"
+          text-color="#fff"
+          active-text-color="#409EFF"
+          unique-opened
+          :collapse="isCollapse"
+          :collapse-transition="false"
+        >
+          <el-submenu
+            :index="item.id + ''"
+            v-for="item in meunList"
+            :key="item.id"
+          >
+            <template slot="title">
+              <i :class="iconObj[item.id]"></i>
+              <span>{{ item.authName }}</span>
+            </template>
+            <el-menu-item
+              :index="subitem.id + ''"
+              v-for="subitem in item.children"
+              :key="subitem.id"
+            >
+              <template slot="title">
+                <i class="el-icon-menu"></i>
+                <span>{{ subitem.authName }}</span>
+              </template>
+            </el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
+      <el-main>Main</el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      meunList: [],
+      iconObj: {
+        "125": "el-icon-s-custom",
+        "103": "el-icon-star-on",
+        "101": "el-icon-s-goods",
+        "102": "el-icon-s-order",
+        "145": "el-icon-s-platform",
+      },
+      isCollapse: false,
+    };
+  },
+  created() {
+    this.getMenuList();
+  },
   methods: {
     logout() {
       window.sessionStorage.clear();
       this.$router.push("/login");
     },
+    async getMenuList() {
+      const { data: res } = await this.$http.get("/menus");
+      console.log(res);
+      if (res.meta.status !== 200) return this.$message.error(res.meta.msg);
+      this.meunList = res.data;
+    },
+    toggleCollapse() {
+      this.isCollapse = !this.isCollapse;
+    },
   },
 };
 </script>
 
-<style lang="less" scoped></style>
+<style lang="less" scoped>
+.el-container {
+  height: 100%;
+  .el-header {
+    background-color: #373d41;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    color: #fff;
+    font-size: 20px;
+  }
+  .el-aside {
+    background-color: #333744;
+    .el-menu {
+      border-right: none;
+    }
+    .toggle_btn {
+      background-color: #4a5064;
+      color: #fff;
+      font-size: 10px;
+      line-height: 24px;
+      text-align: center;
+      letter-spacing: 0.2em;
+      cursor: pointer;
+    }
+  }
+  .el-main {
+    background-color: #eaedf1;
+  }
+}
+</style>
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,5 +1,19 @@
 import Vue from "vue";
-import { Button, Form, FormItem, Input, Message } from "element-ui";
+import {
+  Button,
+  Form,
+  FormItem,
+  Input,
+  Message,
+  Container,
+  Aside,
+  Header,
+  Main,
+  Menu,
+  MenuItem,
+  Submenu,
+  MenuItemGroup,
+} from "element-ui";
 
 Vue.prototype.$message = Message;
 
@@ -7,3 +21,11 @@ Vue.use(Button);
 Vue.use(Form);
 Vue.use(FormItem);
 Vue.use(Input);
+Vue.use(Container);
+Vue.use(Aside);
+Vue.use(Header);
+Vue.use(Main);
+Vue.use(Menu);
+Vue.use(MenuItem);
+Vue.use(Submenu);
+Vue.use(MenuItemGroup);
